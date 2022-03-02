0acdde416328e811f4b9cbca4ab35f84400a560c
Muice <scm1400@gmail.com>
2022-03-02 13:47:04 +0900

    220302 study

---
+++ Spring/core/src/main/java/hello/core/dicount/DiscountPolicy.java
@@ -0,0 +1,13 @@
+package hello.core.dicount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+
+    /**
+     *
+     * @return 할일 대상 금액
+     */
+    int discount(Member member, int price);
+}
